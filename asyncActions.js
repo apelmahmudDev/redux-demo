c8d86681256a19a4df3639517f1be318e125d4f0
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-25 08:14:13 +0600

    Complete the part

--- asyncActions.js
+++ asyncActions.js
@@ -60,15 +60,24 @@ const reducer = (state = initialState, action) => {
 
 const fetchUsers = () => {
 	return function (dispatch) {
+		dispatch(fetchUsersRequest());
 		axios
 			.get("https://jsonplaceholder.typicode.com/users")
 			.then((response) => {
 				// response.data is the array of users
+				const users = response.data.map((user) => user.id);
+				dispatch(fetchUsersSuccess(users));
 			})
 			.catch((error) => {
 				// error.message is the error description
+				dispatch(fetchUsersFailure(error.message));
 			});
 	};
 };
 
 const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+store.dispatch(fetchUsers());
